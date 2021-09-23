8d333c64b09946af5d6f79229113a87c10a65170
KaloyanYosifov <kaloqn665@gmail.com>
2021-09-23 21:58:15 +0200

    Remove experiment code

--- src/kernel.asm
+++ src/kernel.asm
@@ -1,6 +1,5 @@
 [BITS 32]
 global _start
-global help
 extern kernel_start
 
 CODE_SEG equ 0x08
@@ -24,9 +23,5 @@ _start:
 
     call kernel_start
     jmp $
-
-help:
-    mov eax, 65
-    ret
 
 times 512-($ - $$) db 0
--- src/kernel.c
+++ src/kernel.c
@@ -1,18 +1,12 @@
 #include "kernel.h"
 #include "display.h"
 #include "idt/idt.h"
-#include "string/string.h"
 
 extern void problem();
-extern unsigned int help();
 
 void kernel_start() {
     idt_init();
     init_display();
 
-    unsigned int value = help();
-
-    char* str = itoa(value);
-
-    println(str);
+    println("Hello there!");
 }
